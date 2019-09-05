61cb54c2e74ab229bed8f9dbb218da53c2148107
olexster <olexster@gmail.com>
2019-09-05 08:02:40 +0200

    pierwszy

---
+++ plik 1.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+imie = u'Łukasz'
+nazwisko = u'Świątek'
+print (u'Nazywam się ' + imie + ' ' + nazwisko)
+# todo:
+#
---
+++ trojkat.py
@@ -0,0 +1,3 @@
+bok1=bok2=bok3=0.0
+boki = input("podaj trzy boki")
+#todo:
